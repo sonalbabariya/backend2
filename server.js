b915d27971725edb9cd8bfc29f9d3e13446d8915
sonalbabariya <165978727+sonalbabariya@users.noreply.github.com>
2024-09-27 08:16:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://frontend2-navy.vercel.app",
     methods: ["GET","PUT","POST","DELETE"],
     credentials: true,
 }));    
